1a701ac7dcadce8a07f556ed536917e996b369d7
Volker Schwaberow <volker@schwaberow.de>
2025-01-29 06:04:44 +0100

    feat: Import constants in tests module to improve code organization

--- src/tests.rs
+++ src/tests.rs
@@ -2,9 +2,9 @@
 mod tests {
     use super::*;
     use crate::disk::{
-        format_creation_date, load_adf_from_zip, DiskType, ADF, ADF_NUM_SECTORS, ADF_NUM_TRACKS,
-        ADF_SECTOR_SIZE, ADF_TRACK_SIZE, ROOT_BLOCK,
+        format_creation_date, load_adf_from_zip, DiskType, ADF
     };
+    use crate::consts::*;
     use std::{
         io::Write,
         time::{SystemTime, UNIX_EPOCH},
